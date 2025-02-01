38a0c15cad0fea6390253900b0d8201716ef69c3
Chris Hartgerink <chris@libscie.org>
2025-02-01 17:51:53 +0100

    Correct repo

--- vite.config.ts
+++ vite.config.ts
@@ -29,7 +29,7 @@ export default defineConfig(({ command }) => {
     // command === 'build'
     return {
       // build specific config
-      base: '/staple-dashboard/',
+      base: '/dashboard/',
       plugins: [react()],
 
     }
